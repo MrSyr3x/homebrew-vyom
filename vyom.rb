0fc1ae6abeb60d71d0c076ccc66cba82bd3537b1
MrSyr3x <adtygolu134@gmail.com>
2026-01-21 23:42:27 +0530

    Update vyom to v1.0.189

--- vyom.rb
+++ vyom.rb
@@ -1,8 +1,8 @@
 class Vyom < Formula
   desc "Terminal music player for MPD with DSP EQ, synced lyrics, and Cava visualizer"
   homepage "https://github.com/MrSyr3x/Vyom"
-  url "https://github.com/MrSyr3x/Vyom/archive/refs/tags/v1.0.188.tar.gz"
-  sha256 "7220a96aeeae94a17774453364d99350d260b0683247549651b4285a4ec9390d"
+  url "https://github.com/MrSyr3x/Vyom/archive/refs/tags/v1.0.189.tar.gz"
+  sha256 "cbc3a4ddb7041f00387b4e52e4ac43f6edb6053546390657f614e9f7046ed2c6"
   license "MIT"
 
   depends_on "rust" => :build
