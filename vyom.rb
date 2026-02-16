b9c31ca45b0323cde7d3ff9b2aa30a18ab8332b6
MrSyr3x <adtygolu134@gmail.com>
2026-02-16 08:40:25 +0530

    chore: update formula to v1.0.237

--- vyom.rb
+++ vyom.rb
@@ -1,7 +1,7 @@
 class Vyom < Formula
   desc "Terminal music player for MPD with DSP EQ, synced lyrics, and Cava visualizer"
   homepage "https://github.com/MrSyr3x/Vyom"
-  url "https://github.com/MrSyr3x/termony/archive/refs/tags/v1.0.237.tar.gz"
+  url "https://github.com/MrSyr3x/Vyom/archive/refs/tags/v1.0.237.tar.gz"
   sha256 "fec7e7fc4a45ab9cea3d010d5217388640b4dcd43fceb6dc9ba96d3123b71653"
   license "MIT"
 
