ef05c918f3018d6a5c5d2a04d28c78a143172170
MrSyr3x <adtygolu134@gmail.com>
2026-02-17 13:25:12 +0530

    Update vyom to v1.0.242

--- vyom.rb
+++ vyom.rb
@@ -1,7 +1,7 @@
 class Vyom < Formula
   desc "Terminal music player for MPD with DSP EQ, synced lyrics, and Cava visualizer"
   homepage "https://github.com/MrSyr3x/Vyom"
-  url "https://github.com/MrSyr3x/termony/archive/refs/tags/v1.0.242.tar.gz"
+  url "https://github.com/MrSyr3x/Vyom/archive/refs/tags/v1.0.242.tar.gz"
   sha256 "0047e80c8f53ba9e0f4267c9fe4647a73c11df370dfc695ec351f44bc095254d"
   license "MIT"
 
@@ -9,7 +9,6 @@ class Vyom < Formula
 
   # Runtime dependencies
   depends_on "mpd"                         # Required for MPD mode
-  depends_on "cava" => :recommended        # Optional: for visualizer
   depends_on "switchaudio-osx" => :recommended  # Optional: device switching (macOS)
 
   def install
