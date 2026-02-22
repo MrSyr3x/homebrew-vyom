100dcb3d19e13514791d6569ac4fcd2e3537f2a9
MrSyr3x <adtygolu134@gmail.com>
2026-02-22 14:18:02 +0530

    chore: remove git conflict markers from vyom.rb

--- vyom.rb
+++ vyom.rb
@@ -1,11 +1,7 @@
 class Vyom < Formula
   desc "Terminal music player for MPD with DSP EQ, synced lyrics, and Cava visualizer"
   homepage "https://github.com/MrSyr3x/Vyom"
-<<<<<<< HEAD
   url "https://github.com/MrSyr3x/termony/archive/refs/tags/v1.0.248.tar.gz"
-=======
-  url "https://github.com/MrSyr3x/Vyom/archive/refs/tags/v1.0.248.tar.gz"
->>>>>>> 3a36459 (Update vyom to v1.0.248)
   sha256 "47471350726e90d9605259b6e2b98a0abaf6edcc4586e92bf5d8092a7d4bc754"
   license "MIT"
 
